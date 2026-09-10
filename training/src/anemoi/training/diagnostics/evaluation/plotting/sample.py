b23c14352bfd7a41207229e4b85d6561c56fbe0a
Timothy Smith <smith.timothy.andrew@gmail.com>
2026-09-10 13:31:39 +0200

    import datashader lazily

--- training/src/anemoi/training/diagnostics/evaluation/plotting/sample.py
+++ training/src/anemoi/training/diagnostics/evaluation/plotting/sample.py
@@ -7,11 +7,9 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
-import datashader as dsh
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from datashader.mpl_ext import dsshow
 from matplotlib import colormaps as mpl_colormaps
 from matplotlib.collections import PathCollection
 from matplotlib.colors import BoundaryNorm
@@ -130,6 +128,16 @@ def single_plot(
             scatter_kwargs["transform"] = data_crs
         psc = ax.scatter(lon, lat, **scatter_kwargs)
     else:
+        try:
+            import datashader as dsh
+            from datashader.mpl_ext import dsshow
+        except ModuleNotFoundError as e:
+            msg = (
+                "datashader=True requires the datashader package. "
+                "Install it with: pip install anemoi-training[plotting]"
+            )
+            raise ModuleNotFoundError(msg) from e
+
         df = pd.DataFrame({"val": data, "x": lon, "y": lat})
         lower_limit = 25
         upper_limit = 500
